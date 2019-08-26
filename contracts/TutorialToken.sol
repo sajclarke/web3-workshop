ab513176a563dfbabaffe4224e0488e545d9d3e6
PrinceJoshua <atherleyjoshua@gmail.com>
2019-08-26 16:05:20 -0400

    changes

---
+++ contracts/TutorialToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract TutorialToken is ERC20 {
+
+    string public name = "TutorialToken";
+    string public symbol = 'TUT';
+    uint public decimals = 18;
+    
+    uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
+
+    constructor() public {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+}
